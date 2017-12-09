99bba5e97c404a2150f32d4b40cb4928f13fe02f
HrJasn <hrjasn@gmail.com>
2017-12-09 20:57:40 +0800

    置入Line-SDK

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,31 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+
+echo "建置中";
+
+	//header('Location: /line-bot-sdk-php-master/line-bot-sdk-tiny/echo_bot.php');
+    //exit;
+
+/*phpinfo();
+$redis = new Redis();
+$redis->connect("127.0.0.1","6379"); 
+$redis->set("ceshi","hello"); 
+echo $redis->get("ceshi"); */
+
+/*require './vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('cVsroaGRUpS+p4UblzflT7SSczaCCBXxmJjq5T6u2cj+Q5M73X3qVdjlFiEVnHFHdDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCrHzjdgYNmvFGoqWT1jj5PjkJ3HNzhO+KucRDyCCBt+jAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3c7176983193ed3aec81757c0984986c']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379, 3.1);
+$msg = $redis->lPop('msgq');
+
+if ($msg){
+        $json_array = json_decode($msg,true);
+        $response = $bot->pushMessage($json_array["events"][0]["source"]["userId"], $textMessageBuilder);
+		echo $response;
+};
+$redis->close();*/
+
 ?>
-Something is wrong with the XAMPP installation :-(
---
+++ htdocs/line-bot-sdk-php-master/line-bot-sdk-tiny/echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'cVsroaGRUpS+p4UblzflT7SSczaCCBXxmJjq5T6u2cj+Q5M73X3qVdjlFiEVnHFHdDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCrHzjdgYNmvFGoqWT1jj5PjkJ3HNzhO+KucRDyCCBt+jAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '3c7176983193ed3aec81757c0984986c';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
