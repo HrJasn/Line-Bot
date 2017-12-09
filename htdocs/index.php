4bbd84b9ec286b80aeaf7797e3386b88171999a6
HrJasn <hrjasn@gmail.com>
2017-12-09 22:14:34 +0800

    測試Line-API v2語法

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,40 @@
 
 echo "建置中";
 
+/*$access_token ='cVsroaGRUpS+p4UblzflT7SSczaCCBXxmJjq5T6u2cj+Q5M73X3qVdjlFiEVnHFHdDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCrHzjdgYNmvFGoqWT1jj5PjkJ3HNzhO+KucRDyCCBt+jAdB04t89/1O/w1cDnyilFU=';
+//define('TOKEN', '你的Channel Access Token');
+ 
+$json_string = file_get_contents('php://input');
+$json_obj = json_decode($json_string);
+ 
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+         
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+];
+ 
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+curl_close($ch);*/
+
 	//header('Location: /line-bot-sdk-php-master/line-bot-sdk-tiny/echo_bot.php');
     //exit;
 
