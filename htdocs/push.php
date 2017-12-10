b1ec2ad44136836fa8ecb93b2f35726698afdf31
HrJasn <hrjasn@gmail.com>
2017-12-10 20:26:31 +0800

    新增 Line Push API頁面

---
+++ htdocs/push.php
@@ -0,0 +1,34 @@
+<?php
+
+$send_text = $_POST['send_text'];
+
+$access_token = 'bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=';
+$user_id = 'U1ddc6f73c2cad9824edfcd1c1b879bd3';
+
+$response_format_text=[
+	"type"=>"text",
+	"text"=>$send_text
+];
+$post_data=[
+	"to"=>$user_id,
+	"messages"=>[$response_format_text]
+];
+
+$ch = curl_init("https://api.line.me/v2/bot/message/push");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+curl_close($ch);
+
+header('Location: push.html');
+
+?>
---
+++ htdocs/reply.php
@@ -0,0 +1,45 @@
+<?php
+
+$access_token ='bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=';
+//define('TOKEN', '你的Channel Access Token');
+ 
+$json_string = file_get_contents('php://input');
+
+$file = fopen("C:\\web\\Line_log.txt", "a+");
+fwrite($file, $json_string."\n");
+$json_obj = json_decode($json_string);
+ 
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+         
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+];
+fwrite($file, json_encode($post_data)."\n");
+ 
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+fwrite($file, $result."\n"); 
+fclose($file);
+curl_close($ch);
+
+?>
