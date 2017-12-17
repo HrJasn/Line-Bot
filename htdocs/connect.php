beccf120d14b109fa27c80accf04687d61eb5930
HrJasn <hrjasn@gmail.com>
2017-12-17 22:19:45 +0800

    登入Session改用資料庫格式

--- htdocs/connect.php
+++ htdocs/connect.php
@@ -15,17 +15,21 @@
 			mysqli_select_db($db,"line");
 			mysqli_query($db,"SET NAMES 'utf8'");
 			
+			/*$stmt = $db->prepare("SELECT Account,Password,COUNT(Account),Permission FROM users where Account = ?");
+			$stmt->bind_param("s", $id);*/
+			
 			$result = mysqli_query($db,"SELECT Account,Password,COUNT(Account),Permission FROM users where Account = '".$id."'");
 			$row = mysqli_fetch_array($result,MYSQLI_NUM);
 
 			if($row[2] == '0'){
 				$_SESSION["login_alert"] = "無此帳號";
 			}elseif($pw != null && password_verify($pw,$row[1]) && $row[3] == 'Admin'){			
-				$_SESSION["Account"] = $id;
+				$_SESSION["Account"] = $row[0];
 			}else{
 				$_SESSION["login_alert"] = "密碼錯誤";
 			}
 			
+			//$stmt->close();			
 			mysqli_close($db);
 			
 		}else{
