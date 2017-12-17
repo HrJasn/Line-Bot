25b896a0b3aa85f23bffb21415c45c9caf077344
HrJasn <hrjasn@gmail.com>
2017-12-17 20:33:01 +0800

    先放上輸入驗證備用

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -8,6 +8,69 @@
 	
 	if(!empty($_POST['ChooseUser'])){
 		$_SESSION['ChooseUser'] = $_POST['ChooseUser'];
+	}	
+
+	if( !get_magic_quotes_gpc() )
+	{
+		if( is_array($_GET) )
+		{
+			while( list($k, $v) = each($_GET) )
+			{
+				if( is_array($_GET[$k]) )
+				{
+					while( list($k2, $v2) = each($_GET[$k]) )
+					{
+						$_GET[$k][$k2] = addslashes($v2);
+					}
+					@reset($_GET[$k]);
+				}
+				else
+				{
+					$_GET[$k] = addslashes($v);
+				}
+			}
+			@reset($_GET);
+		}
+	 
+		if( is_array($_POST) )
+		{
+			while( list($k, $v) = each($_POST) )
+			{
+				if( is_array($_POST[$k]) )
+				{
+					while( list($k2, $v2) = each($_POST[$k]) )
+					{
+						$_POST[$k][$k2] = addslashes($v2);
+					}
+					@reset($_POST[$k]);
+				}
+				else
+				{
+					$_POST[$k] = addslashes($v);
+				}
+			}
+			@reset($_POST);
+		}
+	 
+		/*if( is_array($_COOKIE) )
+		{
+			while( list($k, $v) = each($_COOKIE) )
+			{
+				if( is_array($_COOKIE[$k]) )
+				{
+					while( list($k2, $v2) = each($_COOKIE[$k]) )
+					{
+						$_COOKIE[$k][$k2] = addslashes($v2);
+					}
+					@reset($_COOKIE[$k]);
+				}
+				else
+				{
+					$_COOKIE[$k] = addslashes($v);
+				}
+			}
+			@reset($_COOKIE);
+		}*/
 	}
 
 ?>
