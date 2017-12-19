2d3edb8334d9f905cba5d5509c95d854674861f8
HrJasn <hrjasn@gmail.com>
2017-12-19 23:00:32 +0800

    發送訊息備註防SQLi

--- htdocs/push.php
+++ htdocs/push.php
@@ -27,17 +27,27 @@ if(!empty($_POST['send_text'])){
 	mysqli_select_db($db,"line");
 	mysqli_query($db,"SET NAMES 'utf8'");
 
-	$channel_id = "1549672834";
-	$channel_secret = "3f330e05765e49b96f1a25a787252779";
+	//$channel_id = "1549672834";
+	//$channel_secret = "3f330e05765e49b96f1a25a787252779";
 	$channel_access_token = "bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=";
 
 	$send_text = $_POST['send_text'];
 
 	$db_res = mysqli_query($db,"SELECT UserID,Sub FROM users WHERE Account LIKE '".$ChooseUser."'");
+	
+	/*$stmt = $db->prepare("SELECT UserID,Sub FROM users WHERE Account LIKE ?");
+	$stmt->bind_param("s", $ChooseUser);	
+	$stmt->execute();
+	$stmt->bind_result($userid,$Sub);*/
 
-	while ($row=mysqli_fetch_array($db_res,MYSQLI_NUM)) {
+	while($row=mysqli_fetch_array($db_res,MYSQLI_NUM)){ /*$stmt->fetch()*/
 		if ($row[1]==1){
 			push($channel_access_token,$row[0],'text',$send_text);
+			/*$json = json_encode($data);
+			$stmt_msg = $db->prepare("INSERT INTO message (src,IP,From_User,Cnt_Type,Cnt_ID,Msg,MsgType,MsgText) VALUES ('send',?,?,'user',?,?,'text',?)");
+			$stmt_msg->bind_param('sssss',$myip,$Account,$userid,$json,$send_text);
+			$stmt_msg->execute();
+			$stmt_msg->close();*/
 			mysqli_query($db,"INSERT INTO message (src,IP,From_User,Cnt_Type,Cnt_ID,Msg,MsgType,MsgText) 
 		VALUES ('send','".$myip."','".$_SESSION["Account"]."','user','".$row[0]."','".$data."','text','".$send_text."')");
 		}
