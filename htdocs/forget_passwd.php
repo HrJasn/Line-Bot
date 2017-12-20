3bd77a1b129890e949d4a5fd7ebdf80ee97db5b7
HrJasn <hrjasn@gmail.com>
2017-12-20 20:15:09 +0800

    警告改為一開始不顯示

--- htdocs/forget_passwd.php
+++ htdocs/forget_passwd.php
@@ -2,47 +2,6 @@
 
 	session_start();
 
-	if(!empty($_POST['id'])){
-		
-		include("db_include.php");
-		include("line.php");
-
-		$channel_access_token = "bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=";
-
-		$db = mysqli_connect($DB_Server,$DB_User,$DB_Passwd);
-		mysqli_select_db($db,"line");
-		mysqli_query($db,"SET NAMES 'utf8'");													
-		
-		$id = $_POST['id'];
-		$pw = substr(md5(rand()),0,8);
-
-		$stmt = $db->prepare("SELECT UserID from users WHERE Account = ?");
-		$stmt->bind_param("s", $id);		
-		$stmt->execute();
-		$stmt->bind_result($userid);
-		$stmt->fetch();
-		$stmt->close();
-
-		if($userid!=null){
-			$stmt = $db->prepare("UPDATE users SET Password = ? WHERE Account = ?");
-			$stmt->bind_param("ss", $hash, $id);
-			$hash = password_hash($pw,PASSWORD_DEFAULT);
-			$stmt->execute();
-			$stmt->close();
-			$pw="您的暫時密碼：".$pw;	
-			push($channel_access_token,$userid,'text',$pw);
-			header('Location: login.php');
-		}else{
-			$_SESSION["forget_alert"] = '無此帳號';
-			header('Location: forget_passwd.php');
-		}
-		
-		//mysqli_free_result($db_res);
-		mysqli_close($db);
-	
-	}else{
-		//$_SESSION["forget_alert"] = '請輸入帳號';
-	}
 ?>
 <!DOCTYPE html>
 <html lang="zh-hans">
@@ -105,3 +64,48 @@ text-align:center;
 </body>
 
 </html>
+<?php
+
+	if(!empty($_POST['id'])){
+		
+		include("db_include.php");
+		include("line.php");
+
+		$channel_access_token = "bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=";
+
+		$db = mysqli_connect($DB_Server,$DB_User,$DB_Passwd);
+		mysqli_select_db($db,"line");
+		mysqli_query($db,"SET NAMES 'utf8'");													
+		
+		$id = $_POST['id'];
+		$pw = substr(md5(rand()),0,8);
+
+		$stmt = $db->prepare("SELECT UserID from users WHERE Account = ?");
+		$stmt->bind_param("s", $id);		
+		$stmt->execute();
+		$stmt->bind_result($userid);
+		$stmt->fetch();
+		$stmt->close();
+
+		if($userid!=null){
+			$stmt = $db->prepare("UPDATE users SET Password = ? WHERE Account = ?");
+			$stmt->bind_param("ss", $hash, $id);
+			$hash = password_hash($pw,PASSWORD_DEFAULT);
+			$stmt->execute();
+			$stmt->close();
+			$pw="您的暫時密碼：".$pw;	
+			push($channel_access_token,$userid,'text',$pw);
+			header('Location: login.php');
+		}else{
+			$_SESSION["forget_alert"] = '無此帳號';
+			header('Location: forget_passwd.php');
+		}
+		
+		//mysqli_free_result($db_res);
+		mysqli_close($db);
+	
+	}else{
+		$_SESSION["forget_alert"] = '請輸入帳號';
+	}
+
+?>
--- htdocs/reset_passwd.php
+++ htdocs/reset_passwd.php
@@ -1,10 +1,79 @@
 <?php
 
 	session_start();
-	
+
 	if($_SESSION["Account"] == null){		
 		header('Location: login.php');
-	}elseif(!empty($_POST['oldpw'])){
+	}
+?>
+<!DOCTYPE html>
+<html lang="zh-hans">
+
+<head>
+
+<meta charset="utf8">
+<title>多啦A夢Line-API 修改密碼</title>
+<meta http-equiv="Pragma" content="private" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="Cache-Control" content="private, max-age=600, pre-check=600" />
+
+<link rel=stylesheet type="text/css" href="css\line-template.css"> 
+<link rel=stylesheet type="text/css" href="css\menu.css"> 
+
+<style>
+
+form{
+position:fixed;
+width:100%;
+top: calc(50% - 100px);
+left:0px;
+text-align:center;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="alert" class="alert" style="display:none;">
+	<script>
+		var alert = document.getElementById("alert");
+		alert.style.display = "<?php echo isset($_SESSION["reset_passwd"])?'':'none';?>";
+		alert.innerText = "<?php echo isset($_SESSION["reset_passwd"])?$_SESSION["reset_passwd"]:'';?>";	
+	</script>
+</div>
+
+<div class="title">修改密碼</div>
+
+	<form name="form" method="post" action="reset_passwd.php">
+
+		<input class="pwd" type="password" name="oldpw" placeholder="舊密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '舊密碼'" /> <br>
+		<input class="pwd" type="password" name="pw" placeholder="新密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '新密碼'"  /> <br>
+		<input class="pwd" type="password" name="pwagn" placeholder="確認新密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '確認新密碼'"  /> <br>
+		<input class="userbtn" type="submit" name="button" value="修改密碼" />
+		<script>document.getElementsByName("oldpw")[0].focus();</script>
+
+	</form>
+
+	<div class="listbtn">
+		<ul class="drop-down-menu">
+			<li><a href="javascript: void(0)">≡</a>
+					<ul>
+						<li><a href="index.php">首頁</a></li>
+						<li><a href="admin.php">管理介面</a></li>
+						<li><a href="logout.php">登出</a></li>
+					</ul>
+			</li>
+		</ul>
+	</div>
+
+</body>
+
+</html>
+<?php
+
+	if(!empty($_POST['oldpw'])){
 		
 		if(!empty($_POST['pw']) && !empty($_POST['pwagn']) && $_POST['pw']==$_POST['pwagn']){
 		
@@ -43,73 +112,8 @@
 			$_SESSION["reset_passwd"] = "新密碼、確認密碼輸入錯誤";
 		}
 	}else{
-		$_SESSION["reset_passwd"] = "未輸入舊密碼";
+		$_SESSION["reset_passwd"] = "請輸入舊密碼";
 		//header('Location: reset_passwd.php');
 	}
+
 ?>
-<!DOCTYPE html>
-<html lang="zh-hans">
-
-<head>
-
-<meta charset="utf8">
-<title>多啦A夢Line-API 修改密碼</title>
-<meta http-equiv="Pragma" content="private" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta http-equiv="Cache-Control" content="private, max-age=600, pre-check=600" />
-
-<link rel=stylesheet type="text/css" href="css\line-template.css"> 
-<link rel=stylesheet type="text/css" href="css\menu.css"> 
-
-<style>
-
-form{
-position:fixed;
-width:100%;
-top: calc(50% - 100px);
-left:0px;
-text-align:center;
-}
-
-</style>
-
-</head>
-
-<body>
-
-<div id="alert" class="alert" style="display:none;">
-	<script>
-		var alert = document.getElementById("alert");
-		alert.style.display = "<?php echo !empty($_SESSION["reset_passwd"])?'':'none';?>";
-		alert.innerText = "<?php echo $_SESSION["reset_passwd"];?>";	
-	</script>
-</div>
-
-<div class="title">修改密碼</div>
-
-	<form name="form" method="post" action="reset_passwd.php">
-
-		<input class="pwd" type="password" name="oldpw" placeholder="舊密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '舊密碼'" /> <br>
-		<input class="pwd" type="password" name="pw" placeholder="新密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '新密碼'"  /> <br>
-		<input class="pwd" type="password" name="pwagn" placeholder="確認新密碼" onfocus="this.placeholder = ''" onblur="this.placeholder = '確認新密碼'"  /> <br>
-		<input class="userbtn" type="submit" name="button" value="修改密碼" />
-		<script>document.getElementsByName("oldpw")[0].focus();</script>
-
-	</form>
-
-	<div class="listbtn">
-		<ul class="drop-down-menu">
-			<li><a href="javascript: void(0)">≡</a>
-					<ul>
-						<li><a href="index.php">首頁</a></li>
-						<li><a href="admin.php">管理介面</a></li>
-						<li><a href="logout.php">登出</a></li>
-					</ul>
-			</li>
-		</ul>
-	</div>
-
-</body>
-
-</html>
-
