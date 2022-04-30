7702aaa4d3b293a258f0921b36e2b717500b5e59
HrJasn <hrjasn@gmail.com>
2022-05-01 00:13:04 +0800

    Remove old test info.
    Remove old test token.

--- htdocs/reply.php
+++ htdocs/reply.php
@@ -3,9 +3,9 @@
 error_reporting(0);
 
 /* 輸入申請的Line Developers 資料  */
-	$channel_id = "1549672834";
-	$channel_secret = "3f330e05765e49b96f1a25a787252779";
-	$channel_access_token = "bxFwr3Y8HcIg2vkudiwGpjVy7bIXcJQqtH0fYIcaTyFD1TFIV3CC8SSDDNkFWql3dDuwpWUwjSV4SqnwEFNMkvkJixqkTajgOR/w9mziLCq0auUlLDOq2cbu42CLBaPG8Z9imBTsNX6A05Kq2cpOvAdB04t89/1O/w1cDnyilFU=";
+	$channel_id = "";
+	$channel_secret = "";
+	$channel_access_token = "";
 
 	$myURL = "https://hrjasn.asuscomm.com/upload/";
 
